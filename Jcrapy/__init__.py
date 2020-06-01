7215f763fcecfbe818dad984cda3ff575806ed57
jacty <jacty0219@qq.com>
2020-06-02 01:37:51 +0800

    Remove twisted version info

--- Jcrapy/__init__.py
+++ Jcrapy/__init__.py
@@ -10,9 +10,6 @@ __version__ = "1.0.0"
 version_info = tuple(int(v) if v.isdigit() else v
                      for v in __version__.split('.'))
 
-from twisted import version as _txv
-twisted_version = (_txv.major, _txv.minor, _txv.micro)
-
 # Declare top-level shortcuts
 from Jcrapy.spiders import Spider
 
