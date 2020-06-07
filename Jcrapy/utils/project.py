109539afbf1a6ad4c16105766a490ea0f09487f6
jacty <jacty0219@qq.com>
2020-06-08 01:37:17 +0800

    Replace closest_Jcrapy_cfg with closest_file()

--- Jcrapy/utils/project.py
+++ Jcrapy/utils/project.py
@@ -4,7 +4,7 @@ import warnings
 from importlib import import_module
 
 from Jcrapy.utils.const import ENVVAR
-from Jcrapy.utils.conf import init_env, closest_jcrapy_cfg
+from Jcrapy.utils.conf import init_env, closest_file
 from Jcrapy.settings import Settings
 
 def inside_project():
@@ -17,22 +17,22 @@ def inside_project():
         else:
             return True
             
-    return bool(closest_jcrapy_cfg())
+    return bool(closest_file())
 
 
 def get_project_settings():
     if ENVVAR not in os.environ:
         init_env()
     #initialize default settings from settings.default_settings
-    settings = Settings()
-    settings_module_path = os.environ.get(ENVVAR)
-    if settings_module_path:
-        settings.setmodule(settings_module_path, priority='project')
+    # settings = Settings()
+    # settings_module_path = os.environ.get(ENVVAR)
+    # if settings_module_path:
+    #     settings.setmodule(settings_module_path, priority='project')
     
-    jcrapy_envvars = {k[7:]: v for k, v in os.environ.items() if
-                        k.startswith('JCRAPY_')}
+    # jcrapy_envvars = {k[7:]: v for k, v in os.environ.items() if
+    #                     k.startswith('JCRAPY_')}
 
-    settings.setdict(jcrapy_envvars, priority='project')
+    # settings.setdict(jcrapy_envvars, priority='project')
     
-    return settings        
+    # return settings        
 
